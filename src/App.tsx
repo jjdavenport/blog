d572e61f8df4a1948c4b1ba62daa83f9276ac8fe
jjdavenport <jordanjamesdavenport@gmail.com>
2025-11-09 15:04:21 +0000

    add icons for light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router";
 import { Wrapper, Main, Nav, Footer } from "./components/content";
 import config from "./assets/config.json";
+import useTheme from "./hooks/useTheme";
 
 function App() {
+  const { darkMode } = useTheme();
   return (
     <>
       <title>{config.title}</title>
       <link
         rel="icon"
         type="image/svg+xml"
-        href={`/${config.title}/${config.icon}`}
+        href={`/${config.title}/${darkMode ? config.iconDarkMode : config.icon}`}
       />
       <Wrapper>
         <Nav config={config} />
